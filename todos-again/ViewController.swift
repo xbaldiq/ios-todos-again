7efed6ef994ae30d73d6a0c2bf96b451734c0d27
Iqbaldhia <iqbaldhiaa@gmail.com>
2022-06-13 15:10:04 +0700

    Initial Commit

---
+++ todos-again/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  todos-again
+//
+//  Created by Aldi on 13/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
